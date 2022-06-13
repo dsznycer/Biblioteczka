3f6ba10cb16acc508345763dcb4a436daef5999c
Dominik Sznycer <biuro@alteca.com.pl>
2022-06-13 22:26:46 +0200

    connection with firebase

--- lib/bookshelf/bookshelfScreen.dart
+++ lib/bookshelf/bookshelfScreen.dart
@@ -6,56 +6,57 @@ class bookShelf extends StatelessWidget {
 
   FirebaseFirestore db = FirebaseFirestore.instance;
 
-  void getData() async {
-    await for (var snapshot in db.collection('log').snapshots()) {
-      for (var info in snapshot.docs) {
-        print(info.data());
-      }
-    }
-  }
+  // void getData() async {
+  //   await for (var snapshot in db.collection('log').snapshots()) {
+  //     for (var info in snapshot.docs) {
+  //       print(info.data());
+  //     }
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(color: Colors.amber),
-              width: 200,
-              height: 200,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(color: Colors.amber),
+                  width: 200,
+                  height: 200,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  child: Container(
+                    width: 150,
+                    height: 50,
+                    color: Colors.green,
+                  ),
+                ),
+                StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                    stream: db.collection('log').snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Text('Jebać sektor gosci');
+                      } else {
+                        var times = snapshot.data!.docs;
+                        List<Text> datasy = [];
+                        for (var i in times) {
+                          final item = i.data().values;
+                          datasy.add(Text(item.toString()));
+                          print(item);
+                        }
+                        return Column(children: datasy);
+                      }
+                    }),
+              ],
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/');
-              },
-              child: Container(
-                width: 150,
-                height: 50,
-                color: Colors.green,
-              ),
-            ),
-            FloatingActionButton(onPressed: () {
-              getData();
-            })
-            // StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-            //     stream: db.collection('log').doc().snapshots(),
-            //     builder: (context, snapshot) {
-            //       if (!snapshot.hasData) {
-            //         return Text('Jebać sektor gosci');
-            //       } else {
-            //         var times = snapshot.data;
-            //         List<Text> datasy = [];
-            //         for (var i in times) {
-            //           print(i.data());
-            //           final item = i.data().toString();
-            //           datasy.add(Text(item));
-            //         }
-            //         return Column(children: datasy);
-            //       }
-            //     }),
-          ],
+          ),
         ),
       ),
     );
